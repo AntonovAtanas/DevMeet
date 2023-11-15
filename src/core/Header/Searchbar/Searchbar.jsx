8876d142ab522d52c9498db3d85fc8fd7fbbd42c
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-15 22:21:22 +0200

    Add responsive shrinking search bar

--- src/core/Header/Searchbar/Searchbar.jsx
+++ src/core/Header/Searchbar/Searchbar.jsx
@@ -2,23 +2,19 @@ import './searchbar.css';
 
 export default function Searchbar() {
     return (
-        <div className="searchbar-wrapper">
-            <form>
-                <div className="search-bar">
-                    <input
-                        className="input-search"
-                        type="search"
-                        placeholder="Search event"
-                        name="search"
-                    />
-                    <button className="search-button" type="submit">
-                        <i
-                            className="fa-solid fa-magnifying-glass"
-                            style={{ color: '#33a394', fontSize: '24px' }}
-                        ></i>
-                    </button>
-                </div>
-            </form>
-        </div>
+        <form className="search-bar">
+            <input
+                className="input-search"
+                type="search"
+                placeholder="Search event"
+                name="search"
+            />
+            <button className="search-button" type="submit">
+                <i
+                    className="fa-solid fa-magnifying-glass"
+                    style={{ color: '#33a394', fontSize: '24px' }}
+                ></i>
+            </button>
+        </form>
     );
 }
