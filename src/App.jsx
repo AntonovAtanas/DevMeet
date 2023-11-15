b842ba10a172e91914ddfa27e9e09192e02f9427
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-15 15:36:28 +0200

    Import app logo and global resetting styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import Header from './core/Header/Header';
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <></>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;
---
+++ src/core/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './header.css';
+
+export default function Header() {
+    return (
+        <header>
+            <div className="logo-search-wrapper">
+                <div className="logo-wrapper">
+                    <img
+                        src="/src/assets/devmeet-temp-logo.jpg"
+                        alt="devmeet-logo"
+                    />
+                </div>
+            </div>
+        </header>
+    );
+}
