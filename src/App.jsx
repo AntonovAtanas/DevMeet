167db0f1f97f02b25fbd8707302deceaa7def604
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-16 16:21:38 +0200

    Add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import Header from './core/Header/Header';
 import Home from './features/Home/Home';
+import Footer from './core/Footer/Footer';
 
 function App() {
-    const [count, setCount] = useState(0);
     return (
         <div className="container">
             <Header />
             <main>
                 <Home />
             </main>
+            <Footer />
         </div>
     );
 }
