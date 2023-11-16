5ed0550c1819b8f112eb3fb3b2d0ed93d3c406a2
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-16 13:37:11 +0200

    Add upcoming events component in home

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from './Hero/Hero';
+import UpcomingEvents from './UpcomingEvents/UpcomingEvents';
 import './home.css';
 
 export default function Home() {
     return (
-        <>
+        <div className="home-wrapper">
             <Hero />
-        </>
+            <UpcomingEvents />
+        </div>
     );
 }
---
+++ src/features/Home/UpcomingEvents/UpcomingEvents.jsx
@@ -0,0 +1,9 @@
+import './upcomingEvents.css';
+
+export default function UpcomingEvents() {
+    return (
+        <div className="upcoming-events">
+            <h2 className="heading-main">Upcoming Events</h2>
+        </div>
+    );
+}
