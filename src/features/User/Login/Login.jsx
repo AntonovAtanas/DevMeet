24f81b9dd8444ddb35a6e80ec5a3ad8125f32d58
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-22 08:51:38 +0200

    Add login form validation

--- src/features/User/Login/Login.jsx
+++ src/features/User/Login/Login.jsx
@@ -1,16 +1,57 @@
-import { useState } from 'react';
+import { isValidElement, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './login.css';
+
+import { inputPatterns } from '../../../shared/input-validation-pattern/input-patterns';
+
 export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
+
+    const [isFormValid, setIsFormValid] = useState({
+        username: false,
+        password: false,
+    });
+
+    const [isInputBlurred, setIsInputBlurred] = useState({
+        username: false,
+        password: false,
+    });
 
     function loginSubmitHandler(e) {
         e.preventDefault();
 
-        console.log(username);
-        console.log(password);
+        console.log(formData);
     }
+
+    function handleInputChange(e) {
+        const { name, value } = e.target;
+
+        const pattern = new RegExp(inputPatterns[name]);
+        const isValidInput = pattern.test(value);
+
+        // update if input field is valid or invalid
+        setIsFormValid({
+            ...isFormValid,
+            [name]: isValidInput,
+        });
+
+        // update form state
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    }
+
+    function handleInputBlur(e) {
+        const inputName = e.target.name;
+
+        setIsInputBlurred({ ...isInputBlurred, [inputName]: true });
+    }
+
     return (
         <div className="auth-form">
             <h1>Sign In</h1>
@@ -24,10 +65,22 @@ export default function Login() {
                         type="text"
                         name="username"
                         placeholder="Username..."
-                        className="default-input"
-                        onChange={(e) => setUsername(e.target.value)}
+                        // className="default-input"
+                        className={
+                            !isFormValid.username && isInputBlurred.username
+                                ? 'default-input error-input'
+                                : 'default-input'
+                        }
+                        pattern={inputPatterns.username}
+                        onChange={handleInputChange}
+                        onBlur={handleInputBlur}
                     />
                 </div>
+                {!isFormValid.username && isInputBlurred.username && (
+                    <span className="error-message">
+                        Username must be between 4-12 characters
+                    </span>
+                )}
                 <div className="input-wrapper">
                     <i
                         className="fa-solid fa-lock"
@@ -37,12 +90,28 @@ export default function Login() {
                         type="password"
                         name="password"
                         placeholder="Password..."
-                        className="default-input"
-                        onChange={(e) => setPassword(e.target.value)}
+                        className={
+                            !isFormValid.password && isInputBlurred.password
+                                ? 'default-input error-input'
+                                : 'default-input'
+                        }
+                        pattern={inputPatterns.password}
+                        onChange={handleInputChange}
+                        onBlur={handleInputBlur}
                     />
                 </div>
+                {!isFormValid.password && isInputBlurred.password && (
+                    <span className="error-message">
+                        Password must be between 4-12 characters
+                    </span>
+                )}
 
-                <button className="button-main auth-button">Sign In</button>
+                <button
+                    className="button-main auth-button"
+                    disabled={!isFormValid.username || !isFormValid.password}
+                >
+                    Sign In
+                </button>
             </form>
             <Link to="/user/register">
                 <p className="auth-redirect">
---
+++ src/shared/input-validation-pattern/input-patterns.js
@@ -0,0 +1,4 @@
+export const inputPatterns = {
+    username: '^[A-Za-z0-9]{4,12}$',
+    password: '^[A-Za-z0-9]{4,12}$',
+};
