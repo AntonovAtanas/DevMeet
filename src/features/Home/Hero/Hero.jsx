d44d596625c9b365a770e835311ae519b64ffcb5
AntonovAtanas <atanas.antonov1@gmail.com>
2023-12-03 17:16:46 +0200

    refactored Hero component with css modules

--- src/features/Home/Hero/Hero.jsx
+++ src/features/Home/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
-import './hero.css';
+import styles from './Hero.module.css';
 
 export default function Hero() {
     return (
-        <div className="hero-wrapper">
-            <div className="hero-content">
-                <h1 className="heading-main">
+        <div className={styles.heroWrapper}>
+            <div className={styles.heroContent}>
+                <h1 className={styles.heroMain}>
                     Scripting Success: Transforming Code into Connections
                 </h1>
                 <p>
@@ -13,11 +13,14 @@ export default function Hero() {
                     events catalyze friendships, creating a dynamic network of
                     collaborative developers.
                 </p>
-                <Link to="/events" className="button-main">
+                <Link
+                    to="/events"
+                    className={`${styles.buttonHero} button-main`}
+                >
                     Find your next event
                 </Link>
             </div>
-            <div className="hero-image">
+            <div className={styles.heroImage}>
                 <a href="#">
                     <img
                         src="https://images.unsplash.com/photo-1528605105345-5344ea20e269?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
