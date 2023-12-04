15e5102c8ed61318021c90e34244cab31d5178de
AntonovAtanas <atanas.antonov1@gmail.com>
2023-12-04 15:12:20 +0200

    Edit date transform util

--- src/shared/utils/date-transform.js
+++ src/shared/utils/date-transform.js
@@ -4,7 +4,7 @@ export default function dateTransform(dateString) {
     const parsedDate = moment(dateString, 'YYYY-MM-DD');
 
     // Format the date to the desired output format
-    const formattedDate = parsedDate.format('dddd, MMMM YYYY');
+    const formattedDate = parsedDate.format('dddd, D MMMM, YYYY');
 
     return formattedDate;
 }
