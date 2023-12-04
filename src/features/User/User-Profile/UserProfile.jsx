30e36c88917fb1d06d7e0df575020c830adfd12d
AntonovAtanas <atanas.antonov1@gmail.com>
2023-12-04 17:14:43 +0200

    Add user profile page

---
+++ src/features/User/User-Profile/UserProfile.jsx
@@ -0,0 +1,41 @@
+import { useContext, useEffect, useState } from 'react';
+
+import styles from './UserProfile.module.css';
+
+import AuthContext from '../../../contexts/authContext';
+import eventsService from '../../../services/events-service';
+
+import LoadingSpinner from '../../../shared/LoadingSpinner/LoadingSpinner';
+import EventCard from '../../../shared/EventCard/EventCard';
+
+export default function UserProfile() {
+    const [goingEvents, setGoingEvents] = useState([]);
+    const [error, setError] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const { userId } = useContext(AuthContext);
+
+    useEffect(() => {
+        eventsService
+            .userGoingEvents(userId)
+            .then(({ events }) => {
+                setGoingEvents(events.data);
+                setIsLoaded(true);
+            })
+            .catch(({ error }) => setError(error));
+    }, []);
+
+    if (!isLoaded) {
+        return <LoadingSpinner />;
+    }
+    return (
+        <div className={styles.profileContainer}>
+            <h1 className={styles.attendingEventsHeader}>Attending events</h1>
+            <div className="allEventsWrapper">
+                {goingEvents.map((event) => (
+                    <EventCard event={event} key={event.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
