4cf05a72f96f86bf80511f07a08599612cd4280e
AntonovAtanas <atanas.antonov1@gmail.com>
2023-12-06 10:44:15 +0200

    Add search bar redirect on submit and set search term state in context

--- src/core/Header/Searchbar/Searchbar.jsx
+++ src/core/Header/Searchbar/Searchbar.jsx
@@ -1,20 +1,40 @@
+import { useContext } from 'react';
 import styles from './Searchbar.module.css';
+import SearchContext from '../../../contexts/searchContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Searchbar() {
+    const navigate = useNavigate();
+    const { setSearchTerm } = useContext(SearchContext);
+
+    function setSearchTermInContext(e) {
+        const searchTerm = e.target.value;
+        setSearchTerm(searchTerm);
+    }
+
+    function onSearchHandler(e) {
+        e.preventDefault();
+        navigate('/search');
+    }
+
     return (
-        <form className={styles.searchBar}>
+        <form
+            className={styles.searchBar}
+            onSubmit={onSearchHandler}
+            onChange={setSearchTermInContext}
+        >
             <input
                 className="default-input"
                 type="search"
                 placeholder="Search event"
                 name="search"
             />
-            <a className={styles.searchButton} type="submit">
+            <button className={styles.searchButton} type="submit">
                 <i
                     className="fa-solid fa-magnifying-glass"
                     style={{ color: '#33a394', fontSize: '24px' }}
                 ></i>
-            </a>
+            </button>
         </form>
     );
 }
