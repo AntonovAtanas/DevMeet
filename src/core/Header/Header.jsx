ea65ac5e8c6cae90cdafb3bef53541df8d6c09e2
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2023-12-08 15:27:57 +0200

    Update Header.jsx

--- src/core/Header/Header.jsx
+++ src/core/Header/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 
 import styles from "./Header.module.css";
 
+import logo from "../../assets/devMeet-logo.png";
+
 import Navigation from "./Navigation/Navigation";
 import Searchbar from "./Searchbar/Searchbar";
 
@@ -11,10 +13,7 @@ export default function Header() {
             <div className={styles.logoSearchWrapper}>
                 <div className={styles.logoWrapper}>
                     <Link to="/">
-                        <img
-                            src="src/assets/devMeet-logo.png"
-                            alt="devmeet-logo"
-                        />
+                        <img src={logo} alt="devmeet-logo" />
                     </Link>
                 </div>
                 <Searchbar />
